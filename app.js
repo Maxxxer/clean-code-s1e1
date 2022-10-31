9983ef9f35405ab28fa5d744fffe785250725149
Maxxxer <polonets.maksim@gmail.com>
2022-10-31 21:24:28 +0300

    Revert "Merge branch 'clean-code-s1e1' of github.com:Maxxxer/clean-code-s1e1 into clean-code-s1e1"
    This reverts commit 754f763ecdb0a434e32edfa69eae9b6d1cfab109, reversing
    changes made to 1adc398a22e221f554622b817f8f43a6cef1f2db.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.querySelector(".button_add");//first button
+var addButton=document.querySelectorAll(".button")[0];//first button
 var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
@@ -22,7 +22,7 @@ var createNewTaskElement=function(taskString){
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
     //label
-    var label=document.createElement("p");//label
+    var label=document.createElement("label");//label
     //input (text)
     var editInput=document.createElement("input");//text
     //button.edit
@@ -32,25 +32,19 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
-listItem.classList.add('item')
-
     label.innerText=taskString;
-    label.className='input-container__task';
+    label.className='task';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
-    checkBox.className="input-container__checkbox";
     editInput.type="text";
-    editInput.className="input-container__input";
+    editInput.className="task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.classList.add("button");
-    editButton.classList.add("button_edit");
+    editButton.className="edit";
 
-    deleteButton.classList.add("button");
-    deleteButton.classList.add("button_delete");
+    deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
-    deleteButtonImg.classList.add("button__img_delete");
     deleteButton.appendChild(deleteButtonImg);
 
 
@@ -88,8 +82,8 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('.input-container__input');
-    var label=listItem.querySelector(".input-container__task");
+    var editInput=listItem.querySelector('input[type=text]');
+    var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".button_edit");
     var containsClass=listItem.classList.contains("editMode");
     //If class of the parent is .editmode
@@ -105,7 +99,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("item_editMode");
+    listItem.classList.toggle("editMode");
 };
 
 
@@ -161,10 +155,9 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
-    // var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var checkBox=taskListItem.querySelector(".input-container__checkbox");
-    var editButton=taskListItem.querySelector(".button_edit");
-    var deleteButton=taskListItem.querySelector(".button_delete");
+    var checkBox=taskListItem.querySelector("input[type=checkbox]");
+    var editButton=taskListItem.querySelector("button.edit");
+    var deleteButton=taskListItem.querySelector("button.delete");
 
 
     //Bind editTask to edit button.
